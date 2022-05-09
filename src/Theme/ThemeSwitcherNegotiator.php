d80dc8b74c93e9bff2819ee4bddeae13d479dbae
ekes <ekes@iskra.net>
2022-05-09 16:49:22 +0200

    Tidied up c&p code.

--- src/Theme/ThemeSwitcherNegotiator.php
+++ src/Theme/ThemeSwitcherNegotiator.php
@@ -2,16 +2,9 @@
 
 namespace Drupal\localgov_microsites_group\Theme;
 
-use Drupal\Component\Plugin\Exception\ContextException;
-use Drupal\Component\Plugin\Exception\MissingValueContextException;
-use Drupal\Core\Condition\ConditionAccessResolverTrait;
-use Drupal\Core\Plugin\Context\ContextHandlerInterface;
-use Drupal\Core\Plugin\Context\ContextRepositoryInterface;
-use Drupal\Core\Plugin\ContextAwarePluginInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Theme\ThemeNegotiatorInterface;
-use Drupal\Core\Routing\AdminContext;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\localgov_microsites_group\Entity\MicrositeGroupInterface;
 
 /**
@@ -19,60 +12,21 @@ use Drupal\localgov_microsites_group\Entity\MicrositeGroupInterface;
  */
 class ThemeSwitcherNegotiator implements ThemeNegotiatorInterface {
 
-  use ConditionAccessResolverTrait;
-
   /**
-   * The route admin context to determine whether a route is an admin one.
+   * System theme configuration.
    *
-   * @var \Drupal\Core\Routing\AdminContext
+   * @var \Drupal\Core\Config\ImmutableConfig
    */
-  protected $adminContext;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * The plugin context handler.
-   *
-   * @var \Drupal\Core\Plugin\Context\ContextHandlerInterface
-   */
-  protected $contextHandler;
-
-  /**
-   * The context manager service.
-   *
-   * @var \Drupal\Core\Plugin\Context\ContextRepositoryInterface
-   */
-  protected $contextRepository;
-
-  /**
-   * The theme to be applied.
-   *
-   * @var string
-   */
-  private $theme;
+  protected $systemTheme;
 
   /**
    * ThemeSwitcherNegotiator constructor.
    *
-   * @param \Drupal\Core\Routing\AdminContext $admin_context
-   *   The route admin context service.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Plugin\Context\ContextHandlerInterface $context_handler
-   *   The ContextHandler for applying contexts to conditions properly.
-   * @param \Drupal\Core\Plugin\Context\ContextRepositoryInterface $context_repository
-   *   The lazy context repository service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
    */
-  public function __construct(AdminContext $admin_context, EntityTypeManagerInterface $entity_type_manager, ContextHandlerInterface $context_handler, ContextRepositoryInterface $context_repository) {
-    $this->adminContext = $admin_context;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->contextHandler = $context_handler;
-    $this->contextRepository = $context_repository;
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->systemTheme = $config_factory->get('system.theme');
   }
 
   /**
@@ -87,7 +41,7 @@ class ThemeSwitcherNegotiator implements ThemeNegotiatorInterface {
    * {@inheritdoc}
    */
   public function determineActiveTheme(RouteMatchInterface $route_match) {
-    return \Drupal::config('system.theme')->get('admin');
+    return $this->systemTheme->get('admin');
   }
 
 }
