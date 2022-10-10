6930ebd3d536115937e2f0233beb22c04aafd402
ekes <ekes@iskra.net>
2022-10-10 13:17:49 +0200

    ginvite test (#174)
    Co-authored-by: Stephen Cox <stephen@agile.coop>
    Co-authored-by: Mark Conroy <mark@mark.ie>

--- tests/src/Functional/GroupInvitationAccessTest.php
+++ tests/src/Functional/GroupInvitationAccessTest.php
@@ -2,8 +2,9 @@
 
 namespace Drupal\Tests\localgov_microsites_group\Functional;
 
-use Drupal\Tests\domain_group\Traits\GroupCreationTrait;
-use Drupal\Tests\domain_group\Traits\InitializeGroupsTrait;
+use Drupal\Core\Test\AssertMailTrait;
+use Drupal\Tests\localgov_microsites_group\Traits\InitializeGroupsTrait;
+use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -13,8 +14,9 @@ use Drupal\Tests\BrowserTestBase;
  */
 class GroupInvitationAccessTest extends BrowserTestBase {
 
-  use GroupCreationTrait;
+  use UserCreationTrait;
   use InitializeGroupsTrait;
+  use AssertMailTrait;
 
   /**
    * Will be removed when issue #3204455 on Domain Site Settings gets merged.
@@ -33,14 +35,11 @@ class GroupInvitationAccessTest extends BrowserTestBase {
    * @var array
    */
   protected static $modules = [
-    'node',
     'block',
     'group',
-    'gnode',
     'domain',
-    'domain_site_settings',
     'domain_group',
-    'views',
+    'localgov_microsites_group',
   ];
 
   /**
@@ -53,70 +52,57 @@ class GroupInvitationAccessTest extends BrowserTestBase {
    *
    * @var \Drupal\user\UserInterface
    */
-  protected $testUser;
+  protected $otherUser;
 
   /**
    * User administrator of group 1.
    *
    * @var \Drupal\user\UserInterface
    */
-  protected $groupAdmin;
+  protected $adminUser;
 
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
-
-    // Create test user.
-    $this->testUser = $this->drupalCreateUser([
-      'access content',
-      'access group overview',
+    $this->ownerUser = $this->createUser();
+    $this->adminUser = $this->createUser();
+    $this->memberUser = $this->createUser();
+    $this->otherUser = $this->createUser();
+    $this->createMicrositeGroups([
+      'uid' => $this->ownerUser->id(),
     ]);
-
-    $this->groupAdmin = $this->drupalCreateUser([
-      'access content',
-      'access group overview',
-    ]);
-
-    // Setup the group types and test groups from the InitializeGroupsTrait.
-    $this->initializeTestGroups(['uid' => $this->groupAdmin->id()]);
-    $this->initializeTestGroupsDomains();
-    $this->initializeTestGroupContent();
-
-    // Allow anonymous to view groups of type A.
-    $this->groupTypeA->getAnonymousRole()->grantPermissions([
-      'view group',
-    ])->save();
-
-    // Allow outsider to view group content article of type A.
-    $this->groupTypeA->getOutsiderRole()->grantPermissions([
-      'view group',
-      'view group_node:article entity',
-    ])->save();
-
-    // Allow member to view, edit, delete group content article of type A.
-    $this->groupTypeA->getMemberRole()->grantPermissions([
-      'view group',
-      'access content overview',
-      'administer group',
-      'administer members',
-    ])->save();
-
+    $this->groups[0]->addMember($this->adminUser, ['group_roles' => 'microsite-admin']);
+    $this->groups[0]->addMember($this->memberUser);
+    $this->createMicrositeGroupsDomains($this->groups);
   }
 
   /**
    * Test content access when unique group access is enabled.
    */
   public function testInvitationPermissions() {
-    $domain_storage = \Drupal::entityTypeManager()->getStorage('domain');
-    $ga1_domain = $domain_storage->load('group_' . $this->groupA1->id());
-    // $ga2_domain = $domain_storage->load('group_' . $this->groupA2->id());
-    $this->drupalLogin($this->groupAdmin);
-    $this->drupalGet($ga1_domain->getPath());
+    $group = $this->groups[0];
+
+    // Admin can check invitations.
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('/group/' . $group->id() . '/invitations');
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($ga1_domain->getPath() . '/group/' . $this->groupA1->id()) . '/invitations';
-    $this->assertSession()->statusCodeEquals(200);
+
+    // Ordinary member can't.
+    $this->drupalLogin($this->memberUser);
+    $this->drupalGet('/group/' . $group->id() . '/invitations');
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet('/group/' . $group->id() . '/content/add/group_invitation');
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Check admin can invite.
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('/group/' . $group->id() . '/content/add/group_invitation');
+    $this->submitForm([
+      'invitee_mail[0][value]' => $this->otherUser->getEmail(),
+    ], 'Save');
+    $this->assertMailString('to', $this->otherUser->getEmail(), 1);
   }
 
 }
