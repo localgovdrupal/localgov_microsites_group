3d6843c6111dfe7d3c618861def75ce4bb4970c6
ekes <ekes@iskra.net>
2023-12-31 14:26:08 +0100

    First stab at doing something with the admin mode.
    Fix admin mode to be on in the control domain at all times.

---
+++ src/EventSubscriber/SiteAdminMode.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\localgov_microsites_group\EventSubscriber;
+
+use Drupal\domain\DomainNegotiatorInterface;
+use Drupal\group_sites\GroupSitesAdminModeInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Fixes admin mode for control domain.
+ */
+class SiteAdminMode implements EventSubscriberInterface {
+
+  /**
+   * Constructs a new SiteAdminMode instance.
+   *
+   */
+  public function __construct(protected DomainNegotiatorInterface $domainNegotiator, protected GroupSitesAdminModeInterface $adminMode) {
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = ['fixAdminMode'];
+    return $events;
+  }
+
+  /**
+   * Fix admin mode for the control site.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The event.
+   */
+  public function fixAdminMode(RequestEvent $event) {
+    if ($this->domainNegotiator->getActiveDomain()->isDefault()) {
+      $this->adminMode->setAdminModeOverride(TRUE);
+    }
+  }
+
+}
