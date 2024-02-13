0e3f4f918cfd14d595c270709d870519f85adbc0
ekes <ekes@iskra.net>
2024-02-13 15:08:13 +0100

    Clarify deprecated version of the context plugin.
    https://github.com/localgovdrupal/localgov_microsites_group/pull/438#discussion_r1481937679

--- src/ContextProvider/DomainGroupContext.php
+++ src/ContextProvider/DomainGroupContext.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\localgov_microsites_group\ContextProvider;
 
+use Drupal\Core\Plugin\Context\EntityContext;
 use Drupal\group_context_domain\Context\GroupFromDomainContext;
 
 /**
@@ -9,4 +10,15 @@ use Drupal\group_context_domain\Context\GroupFromDomainContext;
  *
  * @deprecated
  */
-class DomainGroupContext extends GroupFromDomainContext { }
+class DomainGroupContext extends GroupFromDomainContext {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAvailableContexts() {
+    $context = EntityContext::fromEntityTypeId('group', $this->t('Group from domain (deprecated)'));
+    $context->getContextDefinition()->setDescription($this->t('Provided originally by Domain Group. Returns the group from the domain record if there is one.'));
+    return ['group' => $context];
+  }
+
+}
