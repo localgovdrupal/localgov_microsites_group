12ce439d53b9b6d411145d7694033787f440d778
ekes <ekes@iskra.net>
2024-05-30 11:03:24 +0200

    Fuller check of permissions paths.

--- tests/src/Functional/GroupContentTypeAccessTest.php
+++ tests/src/Functional/GroupContentTypeAccessTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\localgov_microsites_group\Functional;
 
 use Drupal\Core\Test\AssertMailTrait;
-use Drupal\Core\Url;
 use Drupal\domain\DomainInterface;
 use Drupal\localgov_microsites_group\DomainFromGroupTrait;
 use Drupal\Tests\BrowserTestBase;
@@ -48,8 +47,8 @@ class GroupContentTypeAccessTest extends BrowserTestBase {
     'domain',
     'localgov_microsites_group',
     'localgov_microsites_blogs',
-    'localgov_microsites_events',
     'localgov_microsites_directories',
+    'localgov_microsites_events',
   ];
 
   /**
@@ -76,29 +75,38 @@ class GroupContentTypeAccessTest extends BrowserTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
+    // Can test the difference between global and group permissions once per
+    // domain global access is set.
+    $this->controller = $this->createUser();
+    $this->controller->addRole('microsites_controller');
+    $this->controller->save();
     $this->ownerUser = $this->createUser();
-    // Can test the difference between global and group permissions once per domain
-    // global access is set.
-    #$this->adminUser1 = $this->createUser(['create localgov_event content']);
-    #$this->adminUser2 = $this->createUser(['create localgov_event content']);
     $this->adminUser1 = $this->createUser();
+    $this->adminUser1->addRole('microsites_trusted_editor');
+    $this->adminUser1->save();
     $this->adminUser2 = $this->createUser();
     $this->memberUser1 = $this->createUser();
+    $this->memberUser1->addRole('microsites_trusted_editor');
+    $this->memberUser1->save();
     $this->memberUser2 = $this->createUser();
     $this->otherUser = $this->createUser();
     $this->createMicrositeGroups([
       'uid' => $this->ownerUser->id(),
     ]);
     $this->groups[1]->addMember($this->adminUser1, ['group_roles' => 'microsite-admin']);
+    $this->groups[2]->addMember($this->adminUser1, ['group_roles' => 'microsite-admin']);
     $this->groups[2]->addMember($this->adminUser2, ['group_roles' => 'microsite-admin']);
 
     $this->createMicrositeGroupsDomains($this->groups);
   }
 
   /**
-   * Test access to manage content.
+   * Test access to disabled module content types accross users and domains.
+   *
+   * Check with different users, and different domains, confirming caching
+   * and checking any unintential cross domain effects.
    */
-  public function testGroupEventAccess() {
+  public function testUsersDomainsContentTypeAccess() {
 
     $modules = [
       'localgov_microsites_events' => [
@@ -120,7 +128,7 @@ class GroupContentTypeAccessTest extends BrowserTestBase {
           'localgov_blog_post',
         ],
         'status' => 200,
-      ]
+      ],
     ];
 
     $group1 = $this->groups[1];
@@ -130,12 +138,13 @@ class GroupContentTypeAccessTest extends BrowserTestBase {
     assert($group1_domain instanceof DomainInterface);
     assert($group2_domain instanceof DomainInterface);
 
-    // Group 1: Admin user.
-    $this->micrositeDomainLogin($group1_domain, $this->adminUser1);
     // Group 2: Admin user.
-    $this->micrositeDomainLogin($group2_domain, $this->adminUser2);
+    $this->micrositeDomainLogin($group2_domain, $this->adminUser1);
 
     foreach ($modules as $module_name => $module_info) {
+      // Group 1: Admin user.
+      $this->micrositeDomainLogin($group1_domain, $this->adminUser1);
+
       $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/nodes');
       $this->assertSession()->statusCodeEquals(200);
 
@@ -144,12 +153,14 @@ class GroupContentTypeAccessTest extends BrowserTestBase {
         foreach ($check_content['content_types'] as $check_content_type) {
           $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/content/create/group_node%3A' . $check_content_type);
           $this->assertSession()->statusCodeEquals($check_content['status']);
+          // @phpstan-ignore-next-line
           #$this->drupalGet($group1_domain->getUrl() . '/node/add/' . $check_content_type);
+          // @phpstan-ignore-next-line
           #$this->assertSession()->statusCodeEquals($check_content['status']);
         }
       }
 
-      // Disable events.
+      // Disable current module.
       $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/domain-settings');
       $page = $this->getSession()->getPage();
       $events_button = $page->findButton($module_name);
@@ -162,23 +173,147 @@ class GroupContentTypeAccessTest extends BrowserTestBase {
         foreach ($check_content['content_types'] as $check_content_type) {
           $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/content/create/group_node%3A' . $check_content_type);
           $this->assertSession()->statusCodeEquals($check_content['status']);
+          // @phpstan-ignore-next-line
           #$this->drupalGet($group1_domain->getUrl() . '/node/add/' . $check_content_type);
+          // @phpstan-ignore-next-line
           #$this->assertSession()->statusCodeEquals($check_content['status']);
         }
       }
 
-      // @todo when we can logout, login as member and check same.
-
-      // Confirm no effects to other domain.
+      // Check with the same admin user no effect on Group 2.
       foreach ($modules as $check_content) {
         foreach ($check_content['content_types'] as $check_content_type) {
           $this->drupalGet($group2_domain->getUrl() . '/group/' . $group2->id() . '/content/create/group_node%3A' . $check_content_type);
           $this->assertSession()->statusCodeEquals(200);
+          // @phpstan-ignore-next-line
           #$this->drupalGet($group2_domain->getUrl() . '/node/add/' . $check_content_type);
+          // @phpstan-ignore-next-line
           #$this->assertSession()->statusCodeEquals(200);
         }
       }
+  /*
+ * @todo
+      $this->micrositeDomainLogout($group1_domain);
+      $this->micrositeDomainLogin($group1_domain, $this->memberUser1);
+      // Confirm the new permissions.
+      foreach ($modules as $check_content) {
+        foreach ($check_content['content_types'] as $check_content_type) {
+          $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/content/create/group_node%3A' . $check_content_type);
+          $this->assertSession()->statusCodeEquals($check_content['status']);
+          // @phpstan-ignore-next-line
+          #$this->drupalGet($group1_domain->getUrl() . '/node/add/' . $check_content_type);
+          // @phpstan-ignore-next-line
+          #$this->assertSession()->statusCodeEquals($check_content['status']);
+        }
+      }
+      $this->micrositeDomainLogout($group1_domain);
+*/
     }
+  }
+
+  /**
+   * Single test to check content types for all modules.
+   */
+  public function testAllModules() {
+    $group1 = $this->groups[1];
+    $group1_domain = $this->getDomainFromGroup($group1);
+    assert($group1_domain instanceof DomainInterface);
+
+    // Enable modules not already enabled for other tests.
+    \Drupal::service('module_installer')->install([
+      'localgov_microsites_group_term_ui',
+      'localgov_microsites_group_webform',
+      'localgov_microsites_news',
+    ]);
+
+    // Create some group content.
+
+    // All modules start disabled.
+    // Shared paths will be enabled by the first module, so not tested against
+    // the second.
+    $modules = [
+      'localgov_microsites_blogs' => [
+        'paths' => [
+          '/content/create/group_node%3Alocalgov_blog_post',
+          '/content/create/group_node%3Alocalgov_blog_channel',
+          '/taxonomy/localgov_topic/add',
+          // https://github.com/localgovdrupal/localgov_microsites_group/issues/472
+        ],
+        'status' => 403,
+      ],
+      'localgov_microsites_directories' => [
+        'paths' => [
+          '/content/create/group_node%3Alocalgov_directory',
+          '/content/create/group_node%3Alocalgov_directories_page',
+          '/content/create/group_node%3Alocalgov_directories_venue',
+          '/content/create/group_node%3Alocalgov_directory_promo_page',
+          '/directory-facets',
+          '/directory-facets/type/add',
+        ],
+        'status' => 403,
+      ],
+      'localgov_microsites_events' => [
+        'paths' => [
+          '/content/create/group_node%3Alocalgov_event',
+          '/taxonomy/localgov_event_price',
+          '/taxonomy/localgov_event_category',
+          '/taxonomy/localgov_event_locality',
+        ],
+        'status' => 403,
+      ],
+      'localgov_microsites_group_webform' => [
+        'paths' => [
+          '/content/create/group_node%3Alocalgov_webform',
+          // More webform testing following
+          // https://github.com/localgovdrupal/localgov_microsites_group/issues/473
+        ],
+        'status' => 403,
+      ],
+      'localgov_microsites_news' => [
+        'paths' => [
+          '/content/create/group_node%3Alocalgov_newsroom',
+          '/content/create/group_node%3Alocalgov_news_article',
+          // Topic enabled by blogs first.
+        ],
+        'status' => 403,
+      ],
+    ];
+
+    // Run as admin user.
+    $this->micrositeDomainLogin($group1_domain, $this->adminUser1);
+    // Start with all disabled.
+    foreach ($modules as $module_name => $module_info) {
+      // Disable current module.
+      $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/domain-settings');
+      $page = $this->getSession()->getPage();
+      $events_button = $page->findButton($module_name);
+      $this->assertEquals('Disable', $events_button->getValue());
+      $events_button->click();
+      $modules[$module_name]['status'] = 403;
+    }
+
+    reset($modules);
+    do {
+      // Check present permissions.
+      foreach ($modules as $check) {
+        foreach ($check['paths'] as $path) {
+          $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . $path);
+          $this->assertSession()->statusCodeEquals($check['status']);
+        }
+      }
+
+      // Enable next module.
+      $next_module = next($modules);
+      if ($next_module) {
+        $module_name = key($modules);
+        $this->drupalGet($group1_domain->getUrl() . '/group/' . $group1->id() . '/domain-settings');
+        $page = $this->getSession()->getPage();
+        $events_button = $page->findButton($module_name);
+        $this->assertEquals('Enable', $events_button->getValue());
+        $events_button->click();
+        $modules[$module_name]['status'] = 403;
+      }
+    } while ($next_module);
   }
 
 }
