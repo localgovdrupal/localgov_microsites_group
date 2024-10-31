6b9ae4c36ec25c567421488b7c5e313ebba37872
ekes <ekes@iskra.net>
2024-10-31 17:15:15 +0100

    The test context service returns group 1 don't enable it until (or just before) that group is actually created to be able to be returned.

--- tests/src/Kernel/UserRoleTest.php
+++ tests/src/Kernel/UserRoleTest.php
@@ -38,13 +38,6 @@ class UserRoleTest extends GroupKernelTestBase {
   protected function setUp(): void {
     parent::setUp();
 
-    // Set config that group_sites needs to run tests.
-    $this->config('group_sites.settings')
-      ->set('context_provider', '@group_sites_test.hardcoded_group_context:group')
-      ->set('no_site_access_policy', 'group_sites.no_site_access_policy.do_nothing')
-      ->set('site_access_policy', 'group_sites.site_access_policy.single')
-      ->save();
-
     $this->installEntitySchema('group');
     $this->installEntitySchema('group_relationship');
     $this->installEntitySchema('user');
@@ -68,6 +61,12 @@ class UserRoleTest extends GroupKernelTestBase {
    * Test the addition and removal of Trusted Editor role to group members.
    */
   public function testTrustedEditor() {
+    // Set config that group_sites needs to run tests.
+    $this->config('group_sites.settings')
+      ->set('context_provider', '@group_sites_test.hardcoded_group_context:group')
+      ->set('no_site_access_policy', 'group_sites.no_site_access_policy.do_nothing')
+      ->set('site_access_policy', 'group_sites.site_access_policy.single')
+      ->save();
 
     // Create some groups.
     $group1 = Group::create([
